4fc019c42797e143f05b629440170787ed6d3502
ezkemboi <ezrqnkemboi@gmail.com>
2020-08-10 23:57:34 +0300

    hello c#

---
+++ hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace helloWold 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
